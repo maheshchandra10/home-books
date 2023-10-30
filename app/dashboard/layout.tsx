652812a8af5e02a83b8ad22382f4f81232990bce
Luan Campos <luancamposproducoes@gmail.com>
2023-10-29 23:32:41 -0300

    chore: update layout dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,10 @@
 import Sidebar from "@/components/Sidebar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Entre na plataforma",
+  description: "Sua biblioteca virtual",
+};
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return <Sidebar>{children}</Sidebar>;
