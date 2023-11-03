210f8f19b98a6111375921829518445957c40b28
Luan Campos <luancamposproducoes@gmail.com>
2023-11-03 09:36:02 -0300

    chore: add data book

---
+++ books.ts
@@ -0,0 +1,160 @@
+export const data = [
+  {
+    id: 1,
+    name: "Mortie",
+    description:
+      "Toxic effect of tin and its compounds, intentional self-harm, sequela",
+  },
+  {
+    id: 2,
+    name: "Kaylee",
+    description:
+      "Laceration of flexor muscle, fascia and tendon of left index finger at forearm level, sequela",
+  },
+  {
+    id: 3,
+    name: "Lexis",
+    description:
+      "Other injury of intrinsic muscle, fascia and tendon of left index finger at wrist and hand level",
+  },
+  {
+    id: 4,
+    name: "Nadia",
+    description:
+      "Unspecified subluxation of left wrist and hand, subsequent encounter",
+  },
+  {
+    id: 5,
+    name: "Virge",
+    description:
+      "Toxic effect of rattlesnake venom, assault, initial encounter",
+  },
+  {
+    id: 6,
+    name: "Kelcy",
+    description: "Symptomatic postprocedural ovarian failure",
+  },
+  {
+    id: 7,
+    name: "Julia",
+    description: "Laceration of gallbladder, subsequent encounter",
+  },
+  {
+    id: 8,
+    name: "Carin",
+    description: "Secondary noninfectious iridocyclitis",
+  },
+  {
+    id: 9,
+    name: "Adolpho",
+    description:
+      "Other fracture of shaft of right femur, initial encounter for closed fracture",
+  },
+  {
+    id: 10,
+    name: "Vevay",
+    description:
+      "Calculus of bile duct with cholangitis, unspecified, without obstruction",
+  },
+  {
+    id: 11,
+    name: "Dionysus",
+    description: "Idiopathic aseptic necrosis of left toe(s)",
+  },
+  {
+    id: 12,
+    name: "Nonna",
+    description: "Poisoning by antidotes and chelating agents, assault",
+  },
+  {
+    id: 13,
+    name: "Hervey",
+    description:
+      "Poisoning by unspecified psychostimulants, accidental (unintentional), subsequent encounter",
+  },
+  {
+    id: 14,
+    name: "Molli",
+    description: "Superficial foreign body, unspecified lower leg, sequela",
+  },
+  {
+    id: 15,
+    name: "Roderic",
+    description:
+      "Unspecified injury of muscle(s) and tendon(s) of peroneal muscle group at lower leg level",
+  },
+  {
+    id: 16,
+    name: "Esme",
+    description:
+      "Unspecified complication of foreign body accidentally left in body following injection or immunization, sequela",
+  },
+  {
+    id: 17,
+    name: "Ford",
+    description:
+      "Unspecified car occupant injured in collision with fixed or stationary object in nontraffic accident, subsequent encounter",
+  },
+  {
+    id: 18,
+    name: "Doralin",
+    description: "Gestational edema with proteinuria, unspecified trimester",
+  },
+  { id: 19, name: "Tamra", description: "Choroidal rupture" },
+  {
+    id: 20,
+    name: "Cassi",
+    description: "Target of (perceived) adverse discrimination and persecution",
+  },
+  {
+    id: 21,
+    name: "Gregoor",
+    description: "Osteitis deformans in neoplastic diseases, right lower leg",
+  },
+  {
+    id: 22,
+    name: "Siward",
+    description:
+      "Salter-Harris Type IV physeal fracture of upper end of left tibia, sequela",
+  },
+  {
+    id: 23,
+    name: "Mauricio",
+    description: "Benign neoplasm of colon, rectum, anus and anal canal",
+  },
+  {
+    id: 24,
+    name: "Wilburt",
+    description: "Deep phlebothrombosis in pregnancy, first trimester",
+  },
+  {
+    id: 25,
+    name: "Nomi",
+    description:
+      "Unspecified open wound of unspecified back wall of thorax with penetration into thoracic cavity, sequela",
+  },
+  {
+    id: 26,
+    name: "Waylan",
+    description: "Laceration with foreign body, left foot, sequela",
+  },
+  { id: 27, name: "Cathyleen", description: "Laceration of adrenal gland" },
+  {
+    id: 28,
+    name: "Ransell",
+    description: "Unspecified sexually transmitted disease",
+  },
+  {
+    id: 29,
+    name: "Weber",
+    description:
+      "Dislocation of unspecified interphalangeal joint of other finger",
+  },
+  {
+    id: 30,
+    name: "Leela",
+    description: "Posterior corneal pigmentations, bilateral",
+  },
+];
+
+export type TBook = (typeof data)[number];
