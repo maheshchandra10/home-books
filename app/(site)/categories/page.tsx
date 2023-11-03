d994829d7d434e1d87d63f95989bc6dbb434262d
Luan Campos <luancamposproducoes@gmail.com>
2023-11-02 23:59:07 -0300

    chore: initial config page

--- app/(site)/categories/page.tsx
+++ app/(site)/categories/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import Icons from "@/components/Icons";
+
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div className="py-[10px] px-5 w-full bg-white">
+      <header className="flex justify-between w-full">
+        <h1 className="text-2xl text-black font-bold">Dashboard</h1>
+        <button className="w-12 h-12 bg-primary flex justify-center items-center rounded-[10px]">
+          <Icons label="add" />
+        </button>
+      </header>
+    </div>
+  );
 };
 
 export default Categories;
--- app/(site)/dashboard/page.tsx
+++ app/(site)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="gb-white py-[10px] px-5">
       <h1 className="text-white">Home Books - Dashboard</h1>
       <button onClick={() => signOut()}>Logout</button>
     </div>
