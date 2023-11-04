3526ec5b5e37bc6f81382a0df197335a2fb0bbdc
Luan Campos <luancamposproducoes@gmail.com>
2023-11-04 16:04:08 -0300

    chore: create page categories

--- app/(site)/categories/page.tsx
+++ app/(site)/categories/page.tsx
@@ -1,19 +1,96 @@
-import { data } from "@/books";
-import Icons from "@/components/Icons";
+"use client";
 
+import Icons from "@/components/Icons";
+import { ColumnDef } from "@tanstack/react-table";
+import { useEffect, useState } from "react";
+
+import { data, TBook } from "@/books";
 import { DataTable } from "@/components/data-table";
-import { columns } from "@/lib/columns";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Categories = () => {
+  const [books, setBooks] = useState<TBook[]>([]);
+
+  useEffect(() => {
+    setBooks(data);
+  }, []);
+
+  const columns: ColumnDef<TBook>[] = [
+    {
+      accessorKey: "id",
+      header: "#",
+    },
+    {
+      accessorKey: "category",
+      header: "Nome",
+    },
+    {
+      accessorKey: "description",
+      header: "Descrição",
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const book = row.original;
+        const bookId = book.id;
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button className="w-8 h-8 p-0" variant="ghost">
+                <Icons label="more_horiz" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem
+                onClick={() => {
+                  setBooks(
+                    books.filter((book) => {
+                      return book.id !== bookId;
+                    })
+                  );
+                }}
+                className="gap-[10px] hover:cursor-pointer"
+              >
+                <Icons label="delete" />
+                Excluir
+              </DropdownMenuItem>
+              <DropdownMenuItem className="gap-[10px] hover:cursor-pointer">
+                <Icons label="edit" />
+                Editar
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+
   return (
-    <div className="py-[10px] px-5 w-full h-full overflow-y-auto bg-white">
+    <div className="py-[10px] px-5 w-full h-screen overflow-y-auto bg-white">
       <header className="flex justify-between w-full">
         <h1 className="text-2xl text-black font-bold">Categorias</h1>
-        <button className="w-12 h-12 bg-primary flex justify-center items-center rounded-[10px]">
+        <Button
+          className="
+            w-12
+            h-12
+            bg-primary
+            hover:bg-blue-500
+            flex 
+            justify-center 
+            items-center 
+            rounded-[10px]
+          "
+        >
           <Icons label="add" />
-        </button>
+        </Button>
       </header>
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={books} />
     </div>
   );
 };
